9bf73827542a71b0e08c93fe83bbfabb32e8bd29
Eeva-Maria Laiho <eeva-maria@laiho.net>
2018-06-23 17:28:40 +0300

    Tehtävät 3.1-3.6

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1,
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2,
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3,
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4,
+    },
+
+]
+
+app.get('/info', (req, res) => {
+    var html = '<p>Puhelinluettelossa ' + persons.length + ' henkilön tiedot</p>'
+        + new Date().toUTCString();
+    res.send(html)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if ( person ) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? persons.map(n => n.id).sort().reverse()[0] : 1
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (body.name === undefined | body.number === undefined) {
+        return response.status(400).json({error: 'name or number missing'})
+    }
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({error: 'name must be unique'})
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
